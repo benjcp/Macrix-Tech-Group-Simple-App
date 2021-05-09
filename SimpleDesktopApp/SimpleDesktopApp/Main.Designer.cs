fc9136789275c314385efad139b53eddb628bd34
Ben <bclprb@gmail.com>
2021-05-09 16:17:29 +0100

    Implimneted saving and loading data from the User Interface.
    The user can now enter in data from the UI and insert, delete and edit rows from the XML file. They can also cancel any changes being made to the data.

--- SimpleDesktopApp/SimpleDesktopApp/Main.Designer.cs
+++ SimpleDesktopApp/SimpleDesktopApp/Main.Designer.cs
@@ -28,19 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvDataTable = new System.Windows.Forms.DataGridView();
+            this.components = new System.ComponentModel.Container();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvDataTable = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.userBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.userBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dgvDataTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dgvDataTable
-            // 
-            this.dgvDataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDataTable.Location = new System.Drawing.Point(12, 12);
-            this.dgvDataTable.Name = "dgvDataTable";
-            this.dgvDataTable.Size = new System.Drawing.Size(601, 240);
-            this.dgvDataTable.TabIndex = 0;
             // 
             // btnSave
             // 
@@ -62,25 +68,140 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // dgvDataTable
+            // 
+            this.dgvDataTable.AutoGenerateColumns = false;
+            this.dgvDataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn10,
+            this.dataGridViewTextBoxColumn11,
+            this.dataGridViewTextBoxColumn12,
+            this.dataGridViewTextBoxColumn13,
+            this.dataGridViewTextBoxColumn14,
+            this.dataGridViewTextBoxColumn15,
+            this.dataGridViewTextBoxColumn16,
+            this.dataGridViewTextBoxColumn17,
+            this.dataGridViewTextBoxColumn18});
+            this.dgvDataTable.DataSource = this.userBindingSource;
+            this.dgvDataTable.Location = new System.Drawing.Point(12, 25);
+            this.dgvDataTable.Name = "dgvDataTable";
+            this.dgvDataTable.Size = new System.Drawing.Size(978, 227);
+            this.dgvDataTable.TabIndex = 3;
+            this.dgvDataTable.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDataTable_CellValueChanged);
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.DataPropertyName = "FirstName";
+            this.dataGridViewTextBoxColumn10.HeaderText = "FirstName";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            // 
+            // dataGridViewTextBoxColumn11
+            // 
+            this.dataGridViewTextBoxColumn11.DataPropertyName = "LastName";
+            this.dataGridViewTextBoxColumn11.HeaderText = "LastName";
+            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            // 
+            // dataGridViewTextBoxColumn12
+            // 
+            this.dataGridViewTextBoxColumn12.DataPropertyName = "StreetName";
+            this.dataGridViewTextBoxColumn12.HeaderText = "StreetName";
+            this.dataGridViewTextBoxColumn12.Name = "dataGridViewTextBoxColumn12";
+            // 
+            // dataGridViewTextBoxColumn13
+            // 
+            this.dataGridViewTextBoxColumn13.DataPropertyName = "Town";
+            this.dataGridViewTextBoxColumn13.HeaderText = "Town";
+            this.dataGridViewTextBoxColumn13.Name = "dataGridViewTextBoxColumn13";
+            // 
+            // dataGridViewTextBoxColumn14
+            // 
+            this.dataGridViewTextBoxColumn14.DataPropertyName = "PostalCode";
+            this.dataGridViewTextBoxColumn14.HeaderText = "PostalCode";
+            this.dataGridViewTextBoxColumn14.Name = "dataGridViewTextBoxColumn14";
+            // 
+            // dataGridViewTextBoxColumn15
+            // 
+            this.dataGridViewTextBoxColumn15.DataPropertyName = "ApartmentNumber";
+            this.dataGridViewTextBoxColumn15.HeaderText = "ApartmentNumber";
+            this.dataGridViewTextBoxColumn15.Name = "dataGridViewTextBoxColumn15";
+            // 
+            // dataGridViewTextBoxColumn16
+            // 
+            this.dataGridViewTextBoxColumn16.DataPropertyName = "PhoneNumber";
+            this.dataGridViewTextBoxColumn16.HeaderText = "PhoneNumber";
+            this.dataGridViewTextBoxColumn16.Name = "dataGridViewTextBoxColumn16";
+            // 
+            // dataGridViewTextBoxColumn17
+            // 
+            this.dataGridViewTextBoxColumn17.DataPropertyName = "DateOfBirth";
+            this.dataGridViewTextBoxColumn17.HeaderText = "DateOfBirth";
+            this.dataGridViewTextBoxColumn17.Name = "dataGridViewTextBoxColumn17";
+            // 
+            // dataGridViewTextBoxColumn18
+            // 
+            this.dataGridViewTextBoxColumn18.DataPropertyName = "Age";
+            this.dataGridViewTextBoxColumn18.HeaderText = "Age";
+            this.dataGridViewTextBoxColumn18.Name = "dataGridViewTextBoxColumn18";
+            this.dataGridViewTextBoxColumn18.ReadOnly = true;
+            // 
+            // userBindingSource
+            // 
+            this.userBindingSource.DataSource = typeof(SimpleDesktopApp.User);
+            // 
+            // userBindingSource1
+            // 
+            this.userBindingSource1.DataSource = typeof(SimpleDesktopApp.User);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(803, 438);
+            this.ClientSize = new System.Drawing.Size(1059, 629);
+            this.Controls.Add(this.dgvDataTable);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.dgvDataTable);
             this.Name = "Main";
             this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvDataTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dgvDataTable;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn streetNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn townDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn postalCodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apartmentNumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneNumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateOfBirthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.BindingSource userBindingSource;
+        private System.Windows.Forms.BindingSource userBindingSource1;
+        private System.Windows.Forms.DataGridView dgvDataTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn18;
     }
 }
--- SimpleDesktopApp/SimpleDesktopApp/Main.cs
+++ SimpleDesktopApp/SimpleDesktopApp/Main.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,14 +18,95 @@ namespace SimpleDesktopApp
             InitializeComponent();
         }
 
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // Try to load what's already in the XML file.
+            LoadData();
+        }
+
         private void btnSave_Click(object sender, EventArgs e)
         {
+            Console.WriteLine("Printing all entries");
             // Save the data into the XML file.
+
+            List<User> lstUsers = new List<User>();
+            foreach (DataGridViewRow row in dgvDataTable.Rows)
+            {
+                Console.WriteLine(row.DataBoundItem);
+                if (row.DataBoundItem != null)
+                {
+                    lstUsers.Add(row.DataBoundItem as User);
+                }
+            }
+
+            try
+            {
+                XMLHandler.SaveData(lstUsers);
+                DisableControls();
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
             // Reload the data in the DataTable.
+            DialogResult dr = MessageBox.Show("Are you sure you want to discard all changes?", "Discard all changes", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            switch (dr)
+            {
+                case DialogResult.Yes:
+                    LoadData();
+                    break;
+
+                case DialogResult.No:
+                    // Do nothing.
+                    break;
+            }
+        }
+
+        private void LoadData()
+        {
+            DisableControls();
+            // Clear current DataGridView and load data from XML file.
+            List<User> users = XMLHandler.LoadData();
+            dgvDataTable.Rows.Clear();
+            BindingSource bs = userBindingSource;
+
+            foreach (User u in users)
+            {
+                bs.Add(u);
+            }
+        }
+
+        private void DisableControls()
+        {
+            btnSave.Enabled = false;
+            btnCancel.Enabled = false;
+        }
+
+        private void EnableControls()
+        {
+            btnSave.Enabled = true;
+            btnCancel.Enabled = true;
+        }
+
+        private void dgvDataTable_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            // Validate the all columns
+
+        }
+
+        private void dgvDataTable_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            dgvDataTable.Rows[e.RowIndex].ErrorText = string.Empty;
+        }
+
+        private void dgvDataTable_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            EnableControls();
         }
     }
 }
