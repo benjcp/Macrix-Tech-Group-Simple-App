05004083b5d2ad2a23398471b7fdcb931d4d6607
Ben <bclprb@gmail.com>
2021-05-09 23:08:08 +0100

    Added User Controller.
    Added a user controller to handle error messages for validation.

---
+++ SimpleDesktopApp/SimpleDesktopApp/UserController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace SimpleDesktopApp
+{
+    public static class UserController
+    {
+
+        public const string EMPTY_ERROR_MESSAGE = @"Value must not be empty";
+        public const string APARTMENT_NUMBER_ERROR_MESSAGE = @"Value must not be less than 1";
+        public const string PHONE_NUMBER_ERROR_MESSAGE = @"Please enter a valid phone number.";
+        public const string FUTURE_DATETIME_ERROR_MESSAGE = @"Please enter a date that is in the past.";
+        public const string INVALID_DATETIME_ERROR_MESSAGE = @"Please enter a valid DateTime.";
+
+        public static string ValidateField(string colName ,string value)
+        {
+
+            switch (colName)
+            {
+                case "FirstName":
+                case "LastName":
+                case "StreetName":
+                case "Town":
+                case "PostalCode":
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        return EMPTY_ERROR_MESSAGE;
+                    }
+                    break;
+
+                case "ApartmentNumber":
+                    if (!string.IsNullOrEmpty(value) && (!int.TryParse(value, out int result) || result < 1))
+                    {
+                        return APARTMENT_NUMBER_ERROR_MESSAGE;
+                    }
+                    break;
+
+                case "PhoneNumber":
+                    string pattern = @"^[0-9-+]{9,15}$";
+
+                    if (string.IsNullOrEmpty(value) || !Regex.IsMatch(value, pattern))
+                    {
+                        return PHONE_NUMBER_ERROR_MESSAGE;
+                    }
+                    break;
+
+                case "DateOfBirth":
+                    if (DateTime.TryParse(value, out DateTime dateOfBirth))
+                    {
+                        if (dateOfBirth > DateTime.Today)
+                        {
+                            return FUTURE_DATETIME_ERROR_MESSAGE;
+                        }
+                    }
+                    else
+                    {
+                        return INVALID_DATETIME_ERROR_MESSAGE;
+                    }
+                    break;
+                default:
+                    {
+                        return null;
+                    }
+            }
+
+            return null;
+        }
+    }
+}
